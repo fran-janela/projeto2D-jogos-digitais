9e960fff338afb80e7a1300ebb22b2f46ea82b6c
fran-janela <francisco.pinheiro.janela@gmail.com>
2023-04-30 20:37:00 -0300

    change room with fade anim

---
+++ Assets/OfficeFloor1SceneManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfficeFloor1SceneManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject playerBody;
+    public GameObject playerFeet;
+    public GameObject camera;
+    public Animator changePlayerRoomAnim;
+    private Vector2 warpPosition;
+    private Vector2 cameraMinPos;
+    private Vector2 cameraMaxPos;
+    private bool changeRoomActive = false;
+    private bool FadeIn = false;
+    private bool FadeOut = false;
+
+    public void Update(){
+        if (changeRoomActive) {
+            if(FadeIn){
+                playerFeet.GetComponent<AudioSource>().Stop();
+                player.GetComponent<Animator>().SetFloat("Speed", 0f);
+                playerBody.GetComponent<Animator>().SetFloat("Speed", 0f);
+                changePlayerRoomAnim.Play("FadeIn");
+                FadeIn = false;
+                FadeOut = true;
+            }
+            else if(!changePlayerRoomAnim.GetCurrentAnimatorStateInfo(0).IsName("FadeIn") && FadeOut){
+                player.GetComponent<Rigidbody2D>().position = warpPosition;
+                camera.GetComponent<cameraScript>().maxPosition = cameraMaxPos;
+                camera.GetComponent<cameraScript>().minPosition = cameraMinPos;
+                changePlayerRoomAnim.Play("FadeOut");
+                FadeOut = false;
+            }
+            else if(!changePlayerRoomAnim.GetCurrentAnimatorStateInfo(0).IsName("FadeOut") && !FadeOut && !FadeIn){
+                player.GetComponent<PlayerMovement>().enabled = true;
+                changeRoomActive = false;
+            }
+        }
+    }
+
+    public void changeRoom(Vector2 DesireWarpPosition, Vector2 CameraMinPos, Vector2 CameraMaxPos){
+        warpPosition = DesireWarpPosition;
+        cameraMinPos = CameraMinPos;
+        cameraMaxPos = CameraMaxPos;
+        changeRoomActive = true;
+        FadeIn = true;
+    }
+
+}
